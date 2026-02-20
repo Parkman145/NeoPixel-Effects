01374519c9531f2fb66de64a2ae4b16829d70c00
Parkman145 <ParkerLearmonth@gmail.com>
2026-02-20 13:32:37 -0800

    Hue main example

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "CNDA/ndarray.h"
 #include "CNDA/array_utilities.h"
+#include "color.h"
 
 template <typename T>
 cv::Mat Ndarray_to_Mat(const CNDA::Ndarray<T>& arr) {
@@ -19,13 +20,25 @@ cv::Mat Ndarray_to_Mat(const CNDA::Ndarray<T>& arr) {
 
 
 int main() {
-
-    auto arr = CNDA::read_np<uint8_t>("sample.npy");
-
+    
+    
+    
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
+    double hue = 0;
+    double hue_speed = 0.01;
+    RGB color = RGB::from_HSV(hue, 1, 1);
+    auto image = cv::Mat(400, 400, CV_64FC3, cv::Scalar(color.B, color.G, color.R));
+    cv::namedWindow("Display window", cv::WINDOW_NORMAL);
+    
+    while (true){
+        
+        hue = fmod(hue+hue_speed, 1.0);
+        color = RGB::from_HSV(hue, 1, 1)/2.0;
+        image.setTo(cv::Scalar(color.B, color.G, color.R));
+        cv::imshow("Display window", image);
 
-    auto image = Ndarray_to_Mat<uint8_t>(arr);
-    cv::imshow("Display window", image);
-    cv::waitKey(0);
+        if (cv::waitKeyEx(30) == 27) break;
+    }
+    cv::destroyAllWindows();
     return 0;
 }
