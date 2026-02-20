4bea8290f6bb7adb24a3b5b6ca1af1de88fdcac8
Parkman145 <ParkerLearmonth@gmail.com>
2026-02-20 13:31:51 -0800

    Implement RGB::from_HSV()

--- include/color.h
+++ include/color.h
@@ -14,6 +14,31 @@ struct RGB {
         R{static_cast<double>(R)/255},
         G{static_cast<double>(G)/255},
         B{static_cast<double>(B)/255} {}
+
+    RGB(double R, double G , double B) :  R{R}, G{G}, B{B} {}
+
+    
+    static RGB from_HSV(double hue, double saturation, double value){
+        double normalizedHue = std::fmod(hue, 1);
+        double chroma = value*saturation;
+        double X = chroma * (1.0 - std::abs(std::fmod(hue/(1.0/6.0), 2.0) - 1.0));
+        
+        if (normalizedHue < 1.0/6.0){
+            return RGB{chroma, X, 0.0};
+        
+        }else if (normalizedHue < 2.0/6.0){
+            return RGB{X, chroma, 0.0};
+        }else if (normalizedHue < 3.0/6.0){
+            return RGB{0.0, chroma, X};
+        }else if (normalizedHue < 4.0/6.0){
+            return RGB{0.0, X, chroma};
+        }else if (normalizedHue < 5.0/6.0){
+            return RGB{X, 0.0, chroma};
+        }else{
+            return RGB{chroma, 0.0, X};
+        }        
+    }
+
     
     RGB operator+(RGB other) { return RGB(R+other.R, G+other.G, B+other.B); }
     RGB operator-(RGB other) { return RGB(R-other.R, G-other.G, B-other.B); }
