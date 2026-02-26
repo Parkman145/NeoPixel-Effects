07eb66422abf8584cd621fe0cfdd708ee2655fd0
Parkman145 <ParkerLearmonth@gmail.com>
2026-02-26 15:22:25 -0800

    Remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -71,9 +71,5 @@ int main() {
     cv::destroyAllWindows();
 
 
-    // for (int i = 0; i < result.size(); i++ ) {
-    //     std::cout << result[i] << " ";
-    // }
-    // std::cout << "\n";
     return 0;
 }
