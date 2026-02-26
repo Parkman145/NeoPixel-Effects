062d44d1b230ff76d71efa0546cd4086a4297212
Parkman145 <ParkerLearmonth@gmail.com>
2026-02-26 15:26:52 -0800

    Refactor main to follow rules defined in readme.md

--- src/main.cpp
+++ src/main.cpp
@@ -27,13 +27,13 @@ cv::Mat generate_image(std::function<NEOFX::RGB(double, double, double)> func, s
 
     cv::Mat output(res_width, res_height, type);  
 
-    double scale_x = scale * res_width;
-    double scale_y = scale * res_height;
+    double scale_x = scale / res_width;
+    double scale_y = scale / res_height;
 
     for (size_t i = 0; i < res_width; i++) {
-        double x = (static_cast<double>(i)-(res_width/2)) / scale_x ;
+        double x = (static_cast<double>(i)-(res_width/2)) * scale_x ;
         for (size_t j = 0; j < res_height; j++) {
-        double y = (static_cast<double>(j)-(res_height/2)) / scale_y;
+        double y = ((res_height/2)-static_cast<double>(j)) * scale_y;
             NEOFX::RGB color = func(x, y, t);
             
             output.at<cv::Vec3d>(j, i) = cv::Vec3d(color.B, color.G, color.R);
@@ -48,25 +48,29 @@ cv::Mat generate_image(std::function<NEOFX::RGB(double, double, double)> func, s
 
 
 int main() {
+
+    // Constants
     size_t res_width = 200;
     size_t res_height = 200;
     double scale = 1;
+    double speed = 1;
+    int time_interval = 30;
     double t = 1.0;
-    double speed = 0.005;
-    std::chrono::steady_clock clock;
+
 
     // auto ramp = NEOFX::ColorRamp({ NEOFX::black, NEOFX::blue, NEOFX::black, NEOFX::blue, NEOFX::black, NEOFX::blue, NEOFX::black, NEOFX::blue, NEOFX::black }, { 0.000, 0.125, 0.250, 0.375, 0.500, 0.625, 0.750, 0.875, 1.000 });
-    NEOFX::ColorRamp ramp({NEOFX::black, NEOFX::red, NEOFX::black}, {0.0, 0.5, 1.0});
-    auto func = NEOFX::pinwheel(ramp, 1, 1, 0, 0, 0.1);
+    // NEOFX::ColorRamp ramp({NEOFX::black, NEOFX::red, NEOFX::black}, {0.0, 0.5, 1.0});
+    auto ramp = NEOFX::rainbow;
+    auto func = NEOFX::pinwheel(ramp, 1, 1, 0, 0, 0.8);
 
 
 
     while (true){
-        t -= speed;
+        t -= speed/time_interval;
         auto im = generate_image(func, res_width, res_height, scale, t);
         cv::imshow("Display window", im);
 
-        if (cv::waitKeyEx(30) == 27) break;
+        if (cv::waitKeyEx(time_interval) == 27) break;
     }
     cv::destroyAllWindows();
 
